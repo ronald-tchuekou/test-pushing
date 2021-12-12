ec938586b93f2a76c53995d064d4e658c4904114
Patrick23718 <noukimi.patrick@gmail.com>
2021-12-12 14:46:12 +0100

    make it better

--- api/middlewares/authJwt.js
+++ api/middlewares/authJwt.js
@@ -8,7 +8,7 @@ verifyToken = (req, res, next) => {
   let token = req.headers["x-access-token"];
 
   if (!token) {
-    return res.status(403).send({ message: "No token provided!" });
+    return res.status(403).send({ message: "Vous devez vous connecter" });
   }
 
   jwt.verify(token, config.secret, (err, decoded) => {

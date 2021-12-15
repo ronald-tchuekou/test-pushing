49a2bf65448e3364caf5c9a05951228a469be0f1
Patrick23718 <noukimi.patrick@gmail.com>
2021-12-15 14:41:48 +0100

    15/12

--- api/routes/galerie.route.js
+++ api/routes/galerie.route.js
@@ -25,7 +25,7 @@ module.exports = function (app) {
   //   );
   app.delete(
     "/coiffeuse/galerie/:id",
-    [authJwt.verifyToken],
+    [authJwt.verifyToken, authJwt.isCoiffeuse],
     controller.deleteGalerie
   );
 };
